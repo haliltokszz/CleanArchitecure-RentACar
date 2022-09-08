30d747d248f9ba84ab41ee37216d517f38a47462
haliltokszz <toksozhalil@gmail.com>
2022-09-08 14:34:32 +0300

    feature: Model entity added.

---
+++ src/demoProjects/rentACar/Domain/Entities/Model.cs
@@ -0,0 +1,25 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Model : Entity
+{
+    public string Name { get; set; }
+    public decimal DailyPrice { get; set; }
+    public string ImageUrl { get; set; }
+    public Guid BrandId { get; set; }
+    public virtual Brand Brand { get; set; }
+
+    public Model()
+    {
+    }
+    
+    public Model(Guid id, Guid brandId, string name, decimal dailyPrice, string imageUrl) : this()
+    {
+        Id = id;
+        BrandId = brandId;
+        Name = name;
+        DailyPrice = dailyPrice;
+        ImageUrl = imageUrl;
+    }
+}
