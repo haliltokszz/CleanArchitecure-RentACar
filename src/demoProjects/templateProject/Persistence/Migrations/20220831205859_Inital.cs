bef8ed60c367ff34b13e28702a97a6e294ed1063
haliltokszz <toksozhalil@gmail.com>
2022-09-08 10:22:44 +0300

    update: initial migration changed

--- src/demoProjects/templateProject/Persistence/Migrations/20220831205859_Inital.cs
+++ src/demoProjects/templateProject/Persistence/Migrations/20220831205859_Inital.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Persistence.Migrations
 {
-    public partial class Init : Migration
+    public partial class Inital : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -12,8 +13,7 @@ namespace Persistence.Migrations
                 name: "Brands",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
                 constraints: table =>
@@ -24,12 +24,12 @@ namespace Persistence.Migrations
             migrationBuilder.InsertData(
                 table: "Brands",
                 columns: new[] { "Id", "Name" },
-                values: new object[] { 1, "BMW" });
+                values: new object[] { new Guid("20615d06-27b7-4474-a1ab-ad562317a0d8"), "BMW" });
 
             migrationBuilder.InsertData(
                 table: "Brands",
                 columns: new[] { "Id", "Name" },
-                values: new object[] { 2, "Mercedes" });
+                values: new object[] { new Guid("c89c56a0-a390-4da8-b7a2-d1ba115bd8f5"), "Mercedes" });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
