ac27d373e7fe87e86127b192ca3fb55e1f3f6f49
haliltokszz <toksozhalil@gmail.com>
2022-09-08 14:36:30 +0300

    feature: MappingProfiles.cs added for Model entity.

---
+++ src/demoProjects/rentACar/Application/Features/Models/Profiles/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Models.DTOs;
+using AutoMapper;
+using Core.Persistence.Paging;
+using Domain.Entities;
+
+namespace Application.Features.Models.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Model, ModelListDTO>().ForMember(c=>c.BrandName, 
+            opt=>opt.MapFrom(c=>c.Brand.Name)).ReverseMap();
+        CreateMap<IPaginate<Model>, ModelListDTO>().ReverseMap();
+    }
+}
