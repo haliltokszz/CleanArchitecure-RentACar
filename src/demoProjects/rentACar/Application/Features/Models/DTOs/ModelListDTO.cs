6f2387d41ea3fd413531b58af23ee4ddb1a2a999
haliltokszz <toksozhalil@gmail.com>
2022-09-08 14:34:47 +0300

    feature: ModelListDTO.cs added

---
+++ src/demoProjects/rentACar/Application/Features/Models/DTOs/ModelListDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Features.Models.DTOs;
+
+public class ModelListDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string BrandName { get; set; }
+    public decimal DailyPrice { get; set; }
+    public string ImageUrl { get; set; }
+}
