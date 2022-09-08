a03a37a975294d8ce503de29f6f96a3639178694
haliltokszz <toksozhalil@gmail.com>
2022-09-08 14:35:20 +0300

    feature: ModelListModel.cs pageable model added.

---
+++ src/demoProjects/rentACar/Application/Features/Models/Models/ModelListModel.cs
@@ -0,0 +1,9 @@
+﻿using Application.Features.Models.DTOs;
+using Core.Persistence.Paging;
+
+namespace Application.Features.Models.Models;
+
+public class ModelListModel : BasePageableModel
+{
+    public ICollection<ModelListDTO> Items { get; set; }
+}
