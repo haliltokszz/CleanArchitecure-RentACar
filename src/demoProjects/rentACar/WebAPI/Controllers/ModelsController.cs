c4adbfab339ec5f0d8d60c5dc1ac8de31b10389e
haliltokszz <toksozhalil@gmail.com>
2022-09-08 14:36:15 +0300

    feature: ModelsController.cs added

---
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.Models.Queries.GetListModel;
+using Application.Features.Models.Queries.GetListModelByDynamic;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+public class ModelsController: BaseController
+{
+    [HttpGet]
+    public async Task<ActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListModelQuery getListModelQuery = new GetListModelQuery{PageRequest = pageRequest};
+        var result = await Mediator.Send(getListModelQuery);
+        return Ok(result);
+    }
+    
+    [HttpPost("GetLis/tByDynamic")]
+    public async Task<ActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+    {
+        GetListModelByDynamicQuery getListModelByDynamicQuery = new GetListModelByDynamicQuery(){PageRequest = pageRequest, Dynamic = dynamic};
+        var result = await Mediator.Send(getListModelByDynamicQuery);
+        return Ok(result);
+    }
+}
